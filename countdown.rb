a42f1dabfde7f36068dcba7c4be7dd89368a06d1
Marissa Zhong <mwz2101@columbia.edu>
2018-06-25 20:27:25 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(integer)
   while integer >= 0 do
-    puts("#{integer} SECOND#{"S" if integer != 1}")
+    puts("#{integer} SECOND#{if integer != 1 ? "S" : ""})
     integer -= 1
   end
   return "HAPPY NEW YEAR!"
